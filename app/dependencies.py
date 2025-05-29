19389421c4a41c5045f31322d8a137128e448d80
Freddy Bautista <abs0rbmejohan3212@gmail.com>
2025-05-29 16:23:56 -0400

    Docs: changing comments to english for better understanding

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,4 +1,3 @@
-
 import redis.exceptions
 from database import SessionLocal\
 
@@ -9,10 +8,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-REDIS_HOST = os.getenv("REDIS_HOST", "192.168.100.2O") #redis vm IP
+REDIS_HOST = os.getenv("REDIS_HOST", "192.168.100.20") #redis vm IP
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6340))
 
-redis_client_instance = None #every client's Global variable
+redis_client_instance = None # Global variable for the Redis client instance
 
 try:
     #
@@ -20,25 +19,25 @@ try:
         host=REDIS_HOST,
         port=REDIS_PORT,
         db=0,
-        decode_responses=True #Very important: decodes reponses to string bytes
+        decode_responses=True # Decodes responses from bytes to strings
     )
-    temp_redis_client.ping() #ping to verify the connection 
+    temp_redis_client.ping() # Ping to verify the connection
     redis_client_instance = temp_redis_client
-    print(f"Succefully connected to Redis in {REDIS_HOST}:{REDIS_PORT}")
+    print(f"Successfully connected to Redis at {REDIS_HOST}:{REDIS_PORT}")
 except redis.exceptions.ConnectionError as e:
-    print(f"ALERT: cannot connect to Redis at starting the application: {e}")
-    # App contnues but redis_client_instance would be None
-    # The enpoints that depends on redis would be able o handle this.
+    print(f"ALERT: Cannot connect to Redis during application startup: {e}")
+    # App continues but redis_client_instance would be None
+    # The endpoints that depend on Redis will handle this gracefully.
 except Exception as e:
-    print(f"ALERT: An unexpected error has happenend at configuring redis: {e}")
+    print(f"ALERT: An unexpected error occurred while configuring Redis: {e}")
 
 def get_redis_client():
-    """Dpendecia de FastAPI para obtener el cliente de Redis.
-    Laanza una excepcion si e cliente no esta disponible."""
+    """FastAPI dependency to get the Redis client.
+    Raises an exception if the client is not available."""
 
     if redis_client_instance is None:
-        raise HTTPException(status_code=503, detail="Cache service (Redis) not avaliable, the connection has failed starting.")
-    
+        raise HTTPException(status_code=503, detail="Cache service (Redis) not available; the connection failed during startup.")
+
     return redis_client_instance
 
 def get_db():
